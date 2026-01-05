5aac16d2e9a43f5b94c4f1415fd550d62a859fe6
ShadowThijs <logtenbergthijs@yahoo.com>
2026-01-05 22:30:15 +0100

    Fix second warnings

---
+++ ex2/ft_custom_errors.py
@@ -0,0 +1,36 @@
+"""Raise Custom errors."""
+
+class GardenError(Exception):
+    """Custom exception for errors in the garden."""
+
+class PlantError(GardenError):
+    """Custom exception for errors with plants."""
+
+class WaterError(GardenError):
+    """Custom exception for errors with water."""
+
+print("=== Custom Garden Errors Demo ===")
+print("\nTesting PlantError...")
+plant_error: str = "The tomato plant is wilting!"
+water_error: str = "Not enough water in the tank!"
+try:
+    raise PlantError(plant_error)
+except PlantError as e:
+    print(f"Caught PlantError: {e}")
+print("\nTesting WaterError...")
+try:
+    raise WaterError(water_error)
+except WaterError as e:
+    print(f"Caught WaterError: {e}")
+print("\nTesting catching all garden errors...")
+
+print("\nTesting catching all garden errors...")
+try:
+    raise PlantError(plant_error)
+except GardenError as e:
+    print(f"Caught a garden error: {e}")
+
+try:
+    raise WaterError(water_error)
+except GardenError as e:
+    print(f"Caught a garden error: {e}")
